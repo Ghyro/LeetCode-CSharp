078cb4f174c68a2d6f371f22b790ded791f3afc2
Kiryl Korzun <kikoz@vestas.com>
2022-02-06 20:48:42 +0100

    Solved issue: 1

---
+++ LeetCode/Easy/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Easy
+{
+    // Issue: 1
+
+    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    //You can return the answer in any order.
+
+    class TwoSum
+    {
+        public int[] TwoSumAlg(int[] nums, int target)
+        {
+            var hash = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var vl = target - nums[i];
+                if (hash.ContainsKey(vl))
+                    return new[] { hash[vl], i };
+
+                if (!hash.ContainsKey(nums[i]))
+                    hash.Add(nums[i], i);
+            }
+            return null;
+        }
+    }
+}
